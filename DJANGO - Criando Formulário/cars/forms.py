8c7a80b3ada925b31977fc2e73e11d806ebf12be
LuanMFC <luanmatheus198@gmail.com>
2024-01-30 11:52:12 -0300

    DJANGO  - ModelForms

--- DJANGO - Criando Formulário/cars/forms.py
+++ DJANGO - Criando Formulário/cars/forms.py
@@ -1,22 +1,8 @@
 from django import forms
-from cars.models import brand, car
+from cars.models import car
 
-class new_car_form(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(brand.objects.all())
-    year = forms.IntegerField()
-    color = forms.CharField(max_length=200)
-    price = forms.IntegerField()
-    image = forms.ImageField()
-
-    def save(self):
-        new_car = car(
-            model = self.cleaned_data["model"],
-            brand = self.cleaned_data["brand"],
-            year = self.cleaned_data["year"],
-            color = self.cleaned_data["color"],
-            price = self.cleaned_data["price"],
-            image = self.cleaned_data["image"]
-        )
-        new_car.save()
-        return new_car 
+class new_car_form(forms.ModelForm):
+    
+    class Meta:
+        model = car
+        fields = ("__all__")
